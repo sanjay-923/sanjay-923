192d6289c6c68514fec62cfc3d9ba583f6c99a6a
sanjay-923 <83443184+sanjay-923@users.noreply.github.com>
2024-09-11 23:52:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+<script>
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
+    });
+</script>
